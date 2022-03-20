76adcd4a524549232a82f2d73d00c86339c33654
hotamago <nhson2003@outlook.com>
2022-03-20 18:09:17 +0700

    update 2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
-function checkinViewPlay(object_this){
-    let hasThisClass = $(object_this).hasClass("aniSon");
-    let inViewOk = ($(object_this).inView("topOnly") || $(object_this).inView("bottomOnly"));
-    let SumStatus = (inViewOk && !hasThisClass);
-    if(SumStatus) $(object_this).addClass("aniSon");
+function checkinViewPlay(object_this) {
+  let hasThisClass = $(object_this).hasClass("aniSon");
+  let inViewOk =
+    $(object_this).inView("topOnly") || $(object_this).inView("bottomOnly");
+  let SumStatus = inViewOk && !hasThisClass;
+  if (SumStatus) $(object_this).addClass("aniSon");
 }
-function animationLoaded(){
-    $('.son-ani-01').each(function(){
-        checkinViewPlay(this);
-    });
-    $('.auto-image').each(function(){
-        checkinViewPlay(this);
-    });
-    $('.text-box').each(function(){
-        checkinViewPlay(this);
-    });
-    $('.text-box-2').each(function(){
-        checkinViewPlay(this);
-    });
-    $('.btn').each(function(){
-        checkinViewPlay(this);
-    });
+function animationLoaded() {
+  $(".son-ani-01").each(function () {
+    checkinViewPlay(this);
+  });
+  $(".auto-image").each(function () {
+    checkinViewPlay(this);
+  });
+  $(".text-box").each(function () {
+    checkinViewPlay(this);
+  });
+  $(".text-box-2").each(function () {
+    checkinViewPlay(this);
+  });
+  $(".btn").each(function () {
+    checkinViewPlay(this);
+  });
 }
 window.onload = animationLoaded;
-$(window).scroll(function() {
-    animationLoaded();
+$(window).scroll(function () {
+  animationLoaded();
 });
